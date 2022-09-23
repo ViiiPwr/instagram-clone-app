60aeec501a480c5e44f2fd0daea880a8fa1a4907
Yuwen Wang <yuwen.wang@thoughtworks.com>
2022-09-23 09:08:46 +0800

    feat: create profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,15 +1,175 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_app/utils/colors.dart';
+import 'package:instagram_app/utils/utils.dart';
+
+import '../widgets/follow_button.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  final String uid; //current App user
+  const ProfileScreen({Key? key, required this.uid}) : super(key: key);
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  var userData = {};
+  int postLen = 0;
+  int followers = 0;
+  int following = 0;
+  bool isFollowing = false;
+  bool isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              title: Text(userData['username']),
+              centerTitle: false,
+            ),
+            body: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          const CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            backgroundImage: NetworkImage(
+                                'https://miro.medium.com/max/1400/1*o4O4SdRfLDAbX2Z41mtUbQ.png'),
+                            radius: 40,
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  buildStatColumn(postLen, 'posts'),
+                                  buildStatColumn(followers, 'followers'),
+                                  buildStatColumn(following, 'following'),
+                                ],
+                              ))
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          FirebaseAuth.instance.currentUser!.uid == widget.uid
+                              ? FollowButton(
+                                  function: () {},
+                                  backgroundColor: mobileBackgroundColor,
+                                  borderColor: Colors.grey,
+                                  text: 'Sign out',
+                                  textColor: primaryColor)
+                              : isFollowing
+                                  ? FollowButton(
+                                      function: () {},
+                                      backgroundColor: Colors.white,
+                                      borderColor: Colors.grey,
+                                      text: 'Unfollow',
+                                      textColor: Colors.black)
+                                  : FollowButton(
+                                      function: () {},
+                                      backgroundColor: Colors.blue,
+                                      borderColor: Colors.blue,
+                                      text: 'Follow',
+                                      textColor: Colors.white)
+                        ],
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Text(
+                          userData['username'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.only(top: 1),
+                        child: Text(
+                          userData['bio'],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+  }
+
+  getData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      var userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.uid)
+          .get(); // current view profile
+      var postSnap = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('uid',
+              isGreaterThanOrEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get();
+      postLen = postSnap.docs.length;
+      userData = userSnap.data()!;
+      followers = userSnap.data()!['followers'].length;
+      following = userSnap.data()!['following'].length;
+      isFollowing = userSnap.data()!['followers'].contains(FirebaseAuth
+          .instance
+          .currentUser!
+          .uid); // check if current view profile is followed by the app user
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Column buildStatColumn(int num, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          num.toString(),
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4),
+          child: Text(
+            label,
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+              color: Colors.grey,
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_app/screens/profile_screen.dart';
 import 'package:instagram_app/screens/search_screen.dart';
 
 import '../screens/add_post_screen.dart';
@@ -10,6 +12,8 @@ List<Widget> homeScreenItem = [
   FeedScreen(),
   SearchScreen(),
   const AddPostScreen(),
-  Text('444444444444'),
-  Text('55555555555'),
+  const Center(child: Text('fav posts\n暂未建设')),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
